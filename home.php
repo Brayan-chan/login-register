6e34a437cf0d62a43547f43c9aa8ef6703a3a180
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-12-04 01:41:15 -0600

    update

--- home.php
+++ home.php
@@ -20,12 +20,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <title>Home</title>
         <link rel="stylesheet" href="assets/css/home.css">
 </head>
 <body>
 
-    <h1 class="letras">Bienvenido a esta nueva página web</h1>
-    <a href="php/cerrar_sesion.php">Cerrar sesión</a>
+    <h1 class="letras">Bienvenido a esta nueva página web</h1><br>
+    <a href="php/cerrar_sesion.php"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
 </body>
 </html>
